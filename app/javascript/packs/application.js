957b85d53204ec87a9805c297f07df6ec675c069
Nick Alberts <nwalberts5@gmail.com>
2017-10-21 11:15:24 -0400

    that was easy

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  let reactElement = document.getElementById('app')
+
+  if (reactElement) {
+    ReactDOM.render(
+      <h1>Boo yaa</h1>,
+      reactElement
+    )
+  }
+})
